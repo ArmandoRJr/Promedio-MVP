39e3f74690dd8cc1700b591d7259a2468c46f391
Tegh Mehta <tegh.mehta@gmail.com>
2022-09-21 13:01:19 -0400

    react-router

---
+++ Promedio/frontend/src/routes/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Promedio
+      </header>
+      <h1>Promedio</h1>
+    </div>
+  );
+}
+
+export default App;
