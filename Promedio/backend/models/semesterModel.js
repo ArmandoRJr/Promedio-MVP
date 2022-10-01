2f85ac42da0d5911f4acc2d34c04cc49f72965d7
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-09-30 22:16:29 -0400

    Created rough site for testing database inquiries

--- Promedio/backend/models/semesterModel.js
+++ Promedio/backend/models/semesterModel.js
@@ -1,15 +1,16 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
-// const app = express();
+const app = express();
 
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-// mongoose.connect("mongodb://127.0.0.1:27017/testDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/testDB", {useNewUrlParser: true});
 
 const courseSchema = new mongoose.Schema({
     name: {
@@ -38,7 +39,7 @@ name: "MATA37",
 description: "A rigorous introduction to Integral Calculus of one variable and infinite series; strong emphasis on combining theory and applications; further developing of tools for mathematical analysis. Riemann Sum, definite integral, Fundamental Theorem of Calculus, techniques of integration, improper integrals, numerical integration, sequences and series, absolute and conditional convergence of series, convergence tests for series, Taylor polynomials and series, power series and applications."
 });
 
-const winterCourses = [course1, course2, course3];
+const defaultCourses = [course1, course2, course3];
 
 // Please follow 3-letter conventions, i.e.
 // Fall 2022 Semester --> 'f22' for name
@@ -55,36 +56,134 @@ const Semester = mongoose.model("Semester", semesterSchema);
 
 const winterSemester = new Semester({
     name: "w22",
-    courses: winterCourses
+    courses: defaultCourses
 });
 
 // winterSemester.save();
 
-// app.get("/", function(req, res) {
+app.get("/", function(req, res) {
 
-//     Semester.find({}, function(err, foundItems){
-//       if (err) {
-//         console.log(err);
-//       } else if (foundItems.length === 0) 
-//       {
-//         Item.insertMany(defaultItems, function(err) {
-//           if (err) 
-//           {
-//             console.log(err);
-//           } 
-//           else 
-//           {
-//             console.log("Successfully saved default items to DB.");
-//           }
-//         });
+    Course.find({}, function(err, foundCourses){
+        if (err) {
+            console.log(err);
+        } else if (foundCourses.length === 0) 
+        {
+            Course.insertMany(defaultCourses, function(err) {
+            if (err) 
+            {
+                console.log(err);
+            } 
+            else 
+            {
+                console.log("Successfully saved default courses to testDB.");
+            }
+            });
+
+            res.redirect("/");
+        }
+        else
+        {
+            res.render("semester", {semesterTitle: "Promedio", courses: foundCourses});
+            // console.log(`foundSemester.name === ${foundSemester.name}`)
+        }
+    });
+
+// res.render("test", {semesterTitle: "Promedio", });
+
+});
+
+app.post("/", function(req, res){
+
+    const courseName = req.body.newCourse;
+    const semesterName = req.body.semester;
   
-//         res.redirect("/");
-//       }
-//       else
-//       {
-//         res.render("list", {listTitle: "Today", newListItems: foundItems});
-//       }
-//     });
+    const course = new Course({
+      name: courseName
+    });
   
+    if (semesterName === "Promedio")
+    {
+      course.save();
+      res.redirect("/");
+    }
+    else
+    {
+      Semester.findOne({name: semesterName}, function(err, foundSemester){
+        foundSemester.courses.push(course);
+        foundSemester.save();
+      });
   
-//   });
+      res.redirect(`/${semesterName}`);
+    }
+});
+
+app.post("/delete", function(req, res){
+    const courseID = req.body.checkbox;
+    const semesterName = req.body.semesterName;
+  
+    if (semesterName === "Promedio")
+    {
+      Course.findByIdAndRemove(courseID, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Successfully deleted checked course with id: ${courseID}`);
+        }
+      });
+  
+      res.redirect("/");
+    }
+    else
+    {
+      // $pull -- deletes item from 'items' array with query: {_id: itemID}
+      Course.findOneAndUpdate({name: semesterName}, {$pull: {courses: {_id: courseID}}}, function(err, results){
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect(`/${semesterName}`);
+        }
+      });
+    }
+});
+
+app.get("/:customSemesterName", function(req, res){
+    // res.render("list", {semesterTitle: "Work List", newListItems: workItems});
+    const customSemesterName = _.capitalize(req.params.customSemesterName);
+  
+    Semester.findOne({name: customSemesterName}, function(err, foundSemester){
+      if (err) { console.log(err); }
+      else if (!foundSemester) // foundList === null
+      {
+        // Create a new semester
+  
+        const semester = new Semester({
+          name: customSemesterName,
+          courses: []
+        });
+  
+        semester.save();
+  
+        res.redirect(`/${customSemesterName}`);
+        // console.log("Does not exist :(");
+      }
+      else
+      {
+        // Show existing list
+        
+        res.render("semester", {semesterTitle: foundSemester.name, courses: foundSemester.courses});
+      }
+    });
+  
+    // const list = new List({
+    //   name: customListName,
+    //   items: defaultItems
+    // });
+  
+    // list.save();
+  
+  });
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Server started on port ${process.env.PORT || 3000}`);
+});
+  
