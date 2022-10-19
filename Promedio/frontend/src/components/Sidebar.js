457c2d82ca83cbf9b0cc55bb5df39bc4c4c46e3e
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-10-18 23:08:05 -0400

    Made progress on course nav bar functionality

--- Promedio/frontend/src/components/Sidebar.js
+++ Promedio/frontend/src/components/Sidebar.js
@@ -3,9 +3,14 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
+import SubMenu from './SubMenu';
+import { SidebarDataNew } from './SidebarDataNew';
+import { IconContext } from 'react-icons/lib';
+import theme from "../styles/theme"
 
 const Nav = styled.div`
-    background: #15171c;
+    ${'' /* background: #15171c; */}
+    background-color: ${({theme}) => theme.colors.primary };
     height: 80px;
     display: flex;
     justify-content: flex-start;
@@ -21,14 +26,29 @@ const NavIcon = styled(Link)`
     align-items: center;
 `;
 
+const NavTitle = styled(Link)`
+    margin-left: 2rem;
+    margin-right: 2rem;
+    ${'' /* font-size: 2rem; */}
+    flow-direction: row;
+    height: 80px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
 const SidebarNav = styled.nav`
     background: #15171c;
+    ${'' /* background-color: ${({theme}) => theme.colors.primary }; */}
     width: 250px;
-    height: 560px;
+    height: calc(100% - 60px);
     display: flex;
     justify-content: flex-start;
     position: fixed;
-    ${'' /* top: 0; */}
+    top: 60px;
+    overflow-y: scroll;
+    bottom: 0;
+    box-sizing: border-box;
     left: ${({ sidebar }) => (sidebar ? '0%' : '-100%')};
     transition: 350ms;
     z-index: 10;
@@ -45,20 +65,24 @@ const Sidebar = () => {
 
     return (
         <>
-            <Nav>
-                <NavIcon to="#">
-                    <FaIcons.FaBars onClick={showSidebar} />
-                </NavIcon>
-            </Nav>
-            <SidebarNav sidebar={!sidebar} >
-                <SidebarWrap>
-                    <Nav>
+            <IconContext.Provider value={{ color: `${theme.colors.primary_light }` }}>
+                <Nav>
+                    <NavIcon to="#">
+                        <FaIcons.FaBars onClick={showSidebar} />
+                    </NavIcon>
+                    {/* <a href={"https://utsc.calendar.utoronto.ca/course/csca08h3"} target={"_blank"}>CSCA08</a> */}
+                </Nav>
+                <SidebarNav sidebar={sidebar} >
+                    <SidebarWrap>
                         <NavIcon to="#">
                             <AiIcons.AiOutlineClose onClick={showSidebar} />
                         </NavIcon>
-                    </Nav>
-                </SidebarWrap>
-            </SidebarNav>
+                        {SidebarDataNew.map((item, index) => {
+                            return <SubMenu item={item} key={index} />;
+                        })}
+                    </SidebarWrap>
+                </SidebarNav>
+            </IconContext.Provider>
         </>
     );
 }
--- Promedio/frontend/src/components/SidebarData.js
+++ Promedio/frontend/src/components/SidebarData.js
@@ -1,94 +1,94 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
+// import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
 import * as RiIcons from "react-icons/ri";
 
 // CONTAINS MOCK DATA FOR TESTING!!
 
-export const SidebarData = [
+export const SidebarDataNew = [
     {
         title: "University of Toronto",
-        path: "/uoft",
-        icon: <AiIcons.FaUniversity />,
+        path: "/home",
+        icon: <FaIcons.FaUniversity />,
         iconClosed: <RiIcons.RiArrowDownSFill />,
         iconOpened: <RiIcons.RiArrowUpSFill />,
         subNav: [
             {
                 title: "2020: Fall",
-                path: "/uoft/f2020",
+                path: "/home/f2020",
                 icon: <FaIcons.FaFolder />,
                 subNav: [
                     {
                         title: "CSCA08",
-                        path: "/uoft/f2020/csca08",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/f2020/csca08",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                     {
                         title: "CSCA67",
-                        path: "/uoft/f2020/csca67",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/f2020/csca67",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                     {
                         title: "MATA31",
-                        path: "/uoft/f2020/mata31",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/f2020/mata31",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                 ]
             },
             {
                 title: "2021: Winter",
-                path: "/uoft/w2021",
+                path: "/home/w2021",
                 icon: <FaIcons.FaFolder />,
                 subNav: [
                     {
                         title: "CSCA48",
-                        path: "/uoft/w2021/csca48",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/w2021/csca48",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                     {
                         title: "MATA22",
-                        path: "/uoft/w2021/mata22",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/w2021/mata22",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                     {
                         title: "MATA37",
-                        path: "/uoft/w2021/mata37",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/w2021/mata37",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                 ]
             },
             {
                 title: "2021: Summer",
-                path: "/uoft/s2021",
+                path: "/home/s2021",
                 icon: <FaIcons.FaFolder />,
                 subNav: [
                     {
                         title: "CSCB07",
-                        path: "/uoft/s2021/cscb07",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/s2021/cscb07",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                     {
                         title: "CSCB09",
-                        path: "/uoft/s2021/cscb09",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/s2021/cscb09",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                     {
                         title: "CSCB58",
-                        path: "/uoft/s2021/cscb58",
-                        icon: <IoIcons.IoDocumentText />,
+                        path: "/home/s2021/cscb58",
+                        icon: <IoIcons.IoIosDocument />,
                     },
                 ]
             },
             // {
             //     title: "2021: Fall",
-            //     path: "/uoft/f2021",
+            //     path: "/home/f2021",
             //     icon: <FaIcons.FaFolder />,
             //     subNav: [
             //         {
             //             title: "CSCA08",
-            //             path: "/uoft/w2021/csca08",
-            //             icon: <IoIcons.IoDocumentText />,
+            //             path: "/home/w2021/csca08",
+            //             icon: <IoIcons.IoIosDocument />,
             //         },
             //     ]
             // },
