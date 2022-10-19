179b9fc55ef17b4bb94fb2524941c7931fc4fb93
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-10-18 23:08:05 -0400

    Made progress on course nav bar functionality

--- Promedio/frontend/src/routes/Home.js
+++ Promedio/frontend/src/routes/Home.js
@@ -6,8 +6,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const FullWidthDiv = styled.div`
   width: 100%;
-  height: 500px;
-  background-color: ${({theme}) => theme.colors.primary};
+  ${'' /* height: 500px; */}
+  height: 100vh;
+  background-color: ${({theme}) => theme.colors.primary };
   color: ${({theme}) => theme.colors.white};
   padding: 0px;
 `;
@@ -20,32 +21,27 @@ const MarginTopRow = styled.div`
   justify-content: center;
 `;
 
-const LoginButton = styled.button`
-  background-color: ${({theme}) => theme.colors.secondary};
-  color: ${({theme}) => theme.colors.white};
-  cursor: pointer;
-  padding: 10px 20px;
-  margin-right: 20px;
-  border: none;
-  border-radius: 5px;
-  &:hover {
-    opacity: 0.8;
-  }
-  font-size: 1.2rem;
-  font-weight: bold;
-`;
+// const LoginButton = styled.button`
+//   background-color: ${({theme}) => theme.colors.secondary};
+//   color: ${({theme}) => theme.colors.white};
+//   cursor: pointer;
+//   padding: 10px 20px;
+//   margin-right: 20px;
+//   border: none;
+//   border-radius: 5px;
+//   &:hover {
+//     opacity: 0.8;
+//   }
+//   font-size: 1.2rem;
+//   font-weight: bold;
+// `;
 
 function Welcome() {
   const navigate = useNavigate();
 
   return (
     <FullWidthDiv>
-    <Sidebar />
-      {/* <h1>CSCA48.</h1>
-      <MarginTopRow>
-        <LoginButton onClick={() => navigate('/courses')}>Add Course</LoginButton>
-      </MarginTopRow> */}
-      
+      <Sidebar />
     </FullWidthDiv>
   );
 }
